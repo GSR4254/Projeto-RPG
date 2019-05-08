2cdbb67911158fb4f44335dce897826ba6c4cb48
Augusto Ferreira Vieira <augusto.vieira@totvs.com.br>
2019-05-08 15:11:32 -0300

    2% atualizado

--- bancoEx/banco.sql
+++ bancoEx/banco.sql
@@ -5,10 +5,5 @@ use RPG;
 create table aventura(id int primary key Identity , nomeA varchar(50));
 
 create table pers_DeD (id int primary key Identity, nomeP varchar(50),
-Vidamax int, AC int, Srt int, Dex int, Con int, Inte Int, Wis int, Cha int);
-
---insert into aventura values (null, 'Deuses');
-
---insert into pers_DeD values (null, 'Leroy', 20, (select aventura.id from aventura where nomeA = 'Deuses'));
-
---select * from pers_ded;
+Vidamax int, AC int, Srt int, Dex int, Con int, Inte Int, Wis int, Cha int, LVL int, Notas varchar (255), aventura int);
+ALTER TABLE pers_DeD ADD FOREIGN KEY ( id ) REFERENCES aventura ( id );

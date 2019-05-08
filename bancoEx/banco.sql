41b7450869eac0b3563301af0134017008e87acb
Augusto Ferreira Vieira <augusto.vieira@totvs.com.br>
2019-05-08 14:36:06 -0300

    Vou tentar melhor quando der, tive umas ideias

--- bancoEx/banco.sql
+++ bancoEx/banco.sql
@@ -2,12 +2,13 @@ create database RPG;
 
 use RPG;
 
-create table aventura(id int primary key auto_increment, nomeA varchar(50));
+create table aventura(id int primary key Identity , nomeA varchar(50));
 
-create table pers_DeD (id int primary key auto_increment, nomeP varchar(50), vidaMax int, aventura int, foreign key pers_DeD(aventura) references aventura(id));
+create table pers_DeD (id int primary key Identity, nomeP varchar(50),
+Vidamax int, AC int, Srt int, Dex int, Con int, Inte Int, Wis int, Cha int);
 
-insert into aventura values (null, 'Deuses');
+--insert into aventura values (null, 'Deuses');
 
-insert into pers_DeD values (null, 'Leroy', 20, (select aventura.id from aventura where nomeA = 'Deuses'));
+--insert into pers_DeD values (null, 'Leroy', 20, (select aventura.id from aventura where nomeA = 'Deuses'));
 
-select * from pers_ded;
+--select * from pers_ded;
